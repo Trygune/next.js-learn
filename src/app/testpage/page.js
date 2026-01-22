524824c0ecacbfaed9c1ed36014b0f3c80b3dd59
Farbod <Farboddaneshmandfard@gmail.com>
2026-01-22 15:12:15 +0330

    seperating main proj from test env

---
+++ src/app/testpage/page.js
@@ -0,0 +1,43 @@
+import { circle, stack } from '../../styled-system/patterns'
+import { H1, H3, Space } from '@/components/Typography'
+
+const Testpage = () => {
+  // const data = await fetch('https://api.vercel.app/blog')
+  // const posts = await data.json()
+
+  return (
+    <>
+      <div>
+        <Space sp="4" />
+        <H1>React Advanced Next.JS</H1>
+        <div
+          className={stack({
+            align: 'center',
+            justify: 'center',
+            h: 'screen',
+            gap: '8',
+          })}
+        >
+          <div
+            className={circle({
+              size: '100px',
+              bg: 'orange.400',
+              color: 'white',
+            })}
+          >
+            CSS
+          </div>
+
+          <H3>Panda Is Running Hello 🐼!</H3>
+        </div>
+        {/* <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul> */}
+      </div>
+    </>
+  )
+}
+
+export default Testpage
